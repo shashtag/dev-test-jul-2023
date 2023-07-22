7bef1b96e1427590e467b92f9950d643986f7aee
Shashwat Gupta <shashwatsatna@gmail.com>
2023-07-22 22:11:10 +0530

    driver code for ans2

--- ans2/main.go
+++ ans2/main.go
@@ -13,10 +13,15 @@ func main() {
 	fmt.Println(cache)
 	fmt.Println(time.Now().UnixNano())
 
-	cache.Put(1, 1, 1)
-	cache.Put(2, 1, 1)
-	cache.Put(3, 1, 1)
-	cache.Put(4, 1, 1)
-	cache.Put(5, 1, 1)
-	cache.Put(6, 1, 1)
+	cache.Put(1, 311, 223)
+	cache.Get(1)
+	cache.Put(2, 4545, 6867)
+	cache.Put(3, 42, 2)
+	cache.Put(4, 24, 6)
+	cache.Get(4)
+	cache.Put(5, 2323, 1)
+	cache.Get(3)
+	cache.Get(1)
+	cache.Put(6, 434, 5)
+
 }
