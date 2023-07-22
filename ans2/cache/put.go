ebdd279f8d1c7ef4d147aa10ec369c617da14634
Shashwat Gupta <shashwatsatna@gmail.com>
2023-07-22 22:11:52 +0530

    func for puting data in cache

---
+++ ans2/cache/put.go
@@ -0,0 +1,41 @@
+package cache
+
+import (
+	"container/heap"
+	"fmt"
+	"math"
+	"time"
+)
+
+func (c *Cache) Put(key, value, weight int64) {
+	fmt.Println()
+	current_time := time.Now().UnixNano()
+
+	// if key exists, update the value and weight
+	if _, ok := c.items[key]; ok {
+		c.items[key] = cacheData{weight: weight, value: value, last_modified: current_time}
+		fmt.Printf("Item %d updated \n", key)
+		return
+	}
+
+	if len(c.items) == c.capacity {
+
+		for i := 0; i < len(heapDataList); i++ {
+			cacheItem := c.items[heapDataList[i].key]
+
+			heapDataList[i].score = float64(cacheItem.weight) / (math.Log(float64(current_time-cacheItem.last_modified+1)) + 1.00)
+		}
+
+		heap.Init(&heapDataList)
+		poppedKey := heap.Pop(&heapDataList)
+		delete(c.items, poppedKey.(int64))
+		fmt.Printf("Item %d popped out of the chache \n", poppedKey)
+
+	}
+
+	heapDataList = append(heapDataList, heapData{key: key})
+	c.items[key] = cacheData{weight: weight, value: value, last_modified: current_time}
+
+	fmt.Printf("Item %d added to the cache \n", key)
+
+}
